9b4eed1746c923da61a500da73866ab9bb97756a
Lucas Geiger <LUCAS@Lucass-MacBook-Air.local>
2015-08-12 13:33:15 -0400

    cleanup

--- server/serverMain.js
+++ server/serverMain.js
@@ -3,16 +3,13 @@ Meteor.startup(function () {
   worksButNotIdealCollection.remove({});
 });
 
-//Check if the server is receiving the changes
+//Check if the server is receiving the changes (in case its just a UI thing)
 // The changes to IdealCollection are not observable on the server. Only on restarting the server will you see the console.log below
-
-
 worksButNotIdealCollection.find({}).observeChanges({
   added: function (id, fields){
     console.log(fields);
   }
 });
-
 worksButNotIdealCollection.find({}).observeChanges({
   added: function (id, fields){
     console.log(fields);
@@ -23,41 +20,35 @@ Meteor.methods({
     //This is the method which reproduces the bug using collection.aggregate()
     idealButBrokenMethod: function() {
       console.log("idealButBrokenMethod -- Single step Aggregation -- START");
-
       Events.aggregate([
-      {
-        $group : {
+        {
+          $group : {
             _id : "$name",
             new: { $sum: 1},
             count: { $sum: "$value"}
+          }
+        },
+        // IDEALLY Using $out would allow all calculations to happen on the mongo server. The notIdealMethod below, will pull
+        // data back in to the webapp and then return it to mongo.
+        //outputting to collection does not trigger the meteor magic. Which would be ideal. $out seems to be the issue.
+        {
+         $out : "idealcollection"
         }
-      },
-      // CRUCIALLY using $out will allow all calculations to happen on the mongo instance. The notIdealMethod below, will pull
-      // data back in to the webapp and then return it to mongo.
-      {
-        $out : "idealcollection"
-      }
-      //outputting to collection does not trigger the meteor magic. Which would be ideal. $out seems to be the issue.
-
       ]);
       console.log("idealButBrokenMethod -- Single step Aggregation -- END");
     },
-
     //This method does worth but it is expensive since it involves a round trip from mongo to the web app
     worksButNotIdealMethod: function() {
       console.log("START -- worksButNotIdealMethod - 1 Aggregate");
-
       results = Events.aggregate([{
         $group : {
             _id : "$name",
             aggregated: { $sum: "$value"}
         }
       }]);
-
       console.log("END -- worksButNotIdealMethod - 1 Aggregate");
 
-
-      //This section shows the workaround which is necessary.
+      //This section shows the workaround which is necessary to make it reactive. This is very expensive since there is a round trip from the mongo server.
       console.log("START -- worksButNotIdealMethod -- 2 Return to Web app Persist");
       worksButNotIdealCollection.remove({});
       results.forEach(function(el){
@@ -69,5 +60,4 @@ Meteor.methods({
       );
       console.log("END -- worksButNotIdealMethod -- 2 Return to Web app Persist")
     }
-
 });
